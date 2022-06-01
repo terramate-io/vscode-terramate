455a7e5ebb82ec3eb3b3dfd73cd1a9af71d302bf
Tiago Natel <t.nateldemoura@gmail.com>
2022-06-01 16:21:15 +0100

    chore: update package.json

--- src/test/helper.ts
+++ src/test/helper.ts
@@ -27,7 +27,7 @@ export let platformEol: string;
  */
 export async function activate(docUri: vscode.Uri) {
 	// The extensionId is `publisher.name` from package.json
-	const ext = vscode.extensions.getExtension('mineiros.vscode-terramate')!;
+	const ext = vscode.extensions.getExtension('mineiros.terramate')!;
 	await ext.activate();
 	try {
 		doc = await vscode.workspace.openTextDocument(docUri);
